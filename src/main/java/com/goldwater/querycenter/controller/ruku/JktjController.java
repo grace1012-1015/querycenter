04108d54553b4d9a833b32c432edaf2bf33a85a6
guoxuan <guoxuan@jawasoft.com.cn>
2020-11-27 17:40:20 +0800

    modify

--- src/main/java/com/goldwater/querycenter/controller/ruku/JktjController.java
+++ src/main/java/com/goldwater/querycenter/controller/ruku/JktjController.java
@@ -1,9 +1,4 @@
 package com.goldwater.querycenter.controller.ruku;
 
-import com.goldwater.querycenter.service.management.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class JktjController {
-    @Autowired
-    private LoginService loginService;
 }
